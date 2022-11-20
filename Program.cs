97db60576477729cba88029460a39dbc9c8dd5ec
Matthew McKenzie <mwmckenzie@gmail.com>
2022-11-20 10:11:30 +0000

    Initial Addition of User Authorization

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using CollegeDataEditor;
 using CollegeDataEditor.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Azure.Functions.Authentication.WebAssembly;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 var http = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 builder.Services.AddScoped(sp => http);
+builder.Services.AddStaticWebAppsAuthentication();
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
